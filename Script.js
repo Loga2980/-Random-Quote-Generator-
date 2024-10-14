7ed7a0fb163cb25c8b81796eb002e1c60a75552e
Loga2980 <logapriyajagan1980@gmail.com>
2024-10-14 19:39:38 +0530

    Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+// JavaScript to show an alert on page load
+window.onload = function() {
+    alert("Welcome to J.Logapriya's Final Project!");
+}
+
+// Example: Function to display a message when a project is clicked
+function showProjectDetails() {
+    alert("Here is a detailed description of the project.");
+}
